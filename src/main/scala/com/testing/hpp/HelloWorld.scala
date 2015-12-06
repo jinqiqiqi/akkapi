e2cf6da3e5f4dbddb31cd546075da89e03c8b606
kinch <jinqiqiqi@gmail.com>
2015-12-06 23:02:26 +0800

    add imiplicity class and its function.

--- src/main/scala/com/testing/hpp/HelloWorld.scala
+++ src/main/scala/com/testing/hpp/HelloWorld.scala
@@ -1,8 +1,13 @@
 package com.testing.hpp
 
+import com.testing.utils.StringUtils.StringImprovements
+
 /**
   * Created by kinch on 12/4/15.
   */
 object HelloWorld extends App {
     println("Hi world.")
+
+    println("HAL".increment)
+    println("HAL".decrement)
 }
---
+++ src/main/scala/com/testing/utils/StringUtils.scala
@@ -0,0 +1,13 @@
+package com.testing.utils
+
+/**
+  * Created by kinch on 12/6/15.
+  */
+object StringUtils {
+
+  implicit class StringImprovements(val s: String) {
+    def increment = s.map(c => (c + 1).toChar)
+    def decrement = s.map(c => (c - 1).toChar)
+  }
+}
+
