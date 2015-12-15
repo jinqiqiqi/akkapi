d602eb413601255d8a8c3f6dff9e62c8d5122d95
kinch <jinqiqiqi@gmail.com>
2015-12-15 23:33:04 +0800

    add string utils.

--- src/main/scala/test/Adler32Checksum.scala
+++ src/main/scala/test/Adler32Checksum.scala
@@ -7,9 +7,21 @@ object Adler32Checksum {
   val MOD_ADLER = 65521
 
   def main(args: Array[String]) {
-    val sum = adler32sum("Wikipedia")
-    printf("Checksum (int) = %d\n", sum)
-    printf("Checksum (hex) = %s\n", sum.toHexString)
+    val values = List(1,2,3,4,5)
+    for(i <- values) println(matchs(i))
+//    println(matchs(1))
+
+//    val sum = adler32sum("Wikipedia")
+//    printf("Checksum (int) = %d\n", sum)
+//    printf("Checksum (hex) = %s\n", sum.toHexString)
+  }
+
+  def matchs(i: Int): String = {
+    i match {
+      case 1 => "case 1"
+      case 2 => "case 2"
+      case someElse => "get the: "+ someElse
+    }
   }
 
   def adler32sum(s: String): Int = {
