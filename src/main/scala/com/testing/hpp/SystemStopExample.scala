ff03b908a640cde88d03c824e5e3b97f9635a5ce
Eric King <jinqiqiqi@gmail.com>
2015-12-20 12:00:37 +0800

    testing

---
+++ src/main/scala/com/testing/hpp/SystemStopExample.scala
@@ -0,0 +1,22 @@
+package com.testing.hpp
+import akka.actor._
+import scala.language.postfixOps
+
+
+/**
+  * Created by kinch on 12/20/15.
+  */
+class TestActor extends Actor {
+  def receive = {
+    case "hello" => println("hello")
+    case s => println(s)
+  }
+}
+
+object SystemStopExample extends App{
+  val system = ActorSystem("SystemStopExample")
+  val actor = system.actorOf(Props[TestActor], name="test")
+
+  actor ! Kill
+
+}
