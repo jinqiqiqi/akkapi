f417f4017d5c2765d523b0bcda4c213c6868a5df
kinch <jinqiqiqi@gmail.com>
2016-01-19 20:31:35 +0800

    add sample.

---
+++ src/main/scala/com/testing/collection/folderTreatment.scala
@@ -0,0 +1,16 @@
+package com.testing.collection
+
+/**
+  * Created by kinch on 1/13/16.
+  */
+object folderTreatment extends App {
+  val findMax = (x: Int, y: Int) => {
+    Thread.sleep(10)
+    val winner = x max y
+    println(s"Compared $x to $y, $winner was larger;")
+    winner
+  }
+
+  val a = Array.range(0, 50)
+  a.par.reduce(findMax)
+}
