2a910c4ac1398a8e6f5ed92f7a9c8250621672a5
Eric.King <qi.jin@happysports.com>
2016-01-19 15:59:42 +0800

    file samples.

---
+++ src/main/scala/com/testing/test/CreateFile.scala
@@ -0,0 +1,14 @@
+package com.testing.test
+
+import java.io._
+
+/**
+  * Created by kinch on 1/12/16.
+  */
+object CreateFile extends App {
+  val pw = new PrintWriter(new File("hi.txt"))
+  pw.println("你好")
+  pw.println("j")
+  pw.println("jj")
+  pw.close()
+}
