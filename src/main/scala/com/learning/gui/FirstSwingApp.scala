317286759a5c3edc7f95e0f62a5e10937a20473b
qi.jin <qi.jin@happysports.com>
2016-11-14 17:05:21 +0800

    add gui

--- src/main/scala/com/learning/gui/FirstSwingApp.scala
+++ src/main/scala/com/learning/gui/FirstSwingApp.scala
@@ -1,6 +1,8 @@
 package com.learning.gui
 
+
 import scala.swing._
+import scala.swing.event.ButtonClicked
 
 /**
   * Created by kinch on 11/14/16.
@@ -8,8 +10,23 @@ import scala.swing._
 object FirstSwingApp extends SimpleSwingApplication {
   def top = new MainFrame {
     title ="第一个窗口，来自swing"
-    contents = new Button {
-      text = "点击试试"
+    val button = new Button {
+      text = "Click me"
+    }
+    val label = new Label {
+      text = "No button clicks registered"
+    }
+    contents = new BoxPanel(Orientation.Vertical) {
+      contents += button
+      contents += label
+      border = Swing.EmptyBorder(30, 30, 30, 30)
+    }
+    listenTo(button)
+    var nClicks = 0
+    reactions += {
+      case ButtonClicked(b) =>
+        nClicks += 1
+        label.text = "Click: "+ nClicks
     }
   }
 }
